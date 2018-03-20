f3c9781c747e4e922d9929cfeb69be770938774a
AlehMeshchanka <teraom@inbox.ru>
2018-03-20 11:33:27 +0300

    maven validate test good

--- web/src/main/java/by/itacademy/controllers/ErrorController.java
+++ web/src/main/java/by/itacademy/controllers/ErrorController.java
@@ -3,9 +3,7 @@ package by.itacademy.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/error")
